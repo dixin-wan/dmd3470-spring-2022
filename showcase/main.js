ad0640b3710d63fec96bf7cce97725148b1ca8b2
dixin-wan <78285322+dixin-wan@users.noreply.github.com>
2022-03-07 15:08:12 -0500

    p

--- showcase/main.js
+++ showcase/main.js
@@ -1,6 +1,9 @@
 var content = document.getElementsByTagName("body")[0];
+var content2 = document.getElementsByTagName("triplet")[0];
 var darkmode = document.getElementById("dark-change");
 darkmode.addEventListener('click',function(){
     darkmode.classList.toggle('active');
     content.classList.toggle('night');
 })
+
+
